eaa9bb4b5abc89af4b002f8101da0b5c00a268fb
tanakaderoy <tanaka.deroy@gmail.com>
2020-06-24 23:50:54 -0400

    create collection view cells

--- Food Delivery/ContentView.swift
+++ Food Delivery/ContentView.swift
@@ -8,6 +8,51 @@
 
 import SwiftUI
 
+class FoodController:  UICollectionViewController {
+
+    private let cellID = "cellId"
+
+
+    init() {
+        super.init(collectionViewLayout: FoodController.createLayout())
+    }
+
+    static func createLayout() -> UICollectionViewCompositionalLayout {
+        return UICollectionViewCompositionalLayout { (sectionNumber, env) -> NSCollectionLayoutSection? in
+            let item = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1)))
+            item.contentInsets.trailing = 16
+            item.contentInsets.bottom = 16
+            let group = NSCollectionLayoutGroup.horizontal(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .absolute(200)), subitems: [item])
+            let section = NSCollectionLayoutSection(group: group)
+            return section
+        }
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellID, for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.backgroundColor = .white
+        navigationItem.title = "Food Delivery"
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellID)
+    }
+
+
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
+
 struct ContentView: View {
     var body: some View {
         Text("Hello, World!")
@@ -16,6 +61,18 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Container().edgesIgnoringSafeArea(.all)
+    }
+
+    struct Container: UIViewControllerRepresentable {
+        func makeUIViewController(context: Context) ->  UIViewController {
+            return UINavigationController(rootViewController: FoodController())
+        }
+
+        func updateUIViewController(_ uiViewController:  UIViewController, context: Context) {
+
+        }
+        typealias UIViewControllerType = UIViewController
+
     }
 }
